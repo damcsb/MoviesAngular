5a6df7221dc6f54b4d0203f0f00937f116d036b9
damcsb <damcsb1@gmail.com>
2020-02-03 13:35:09 +0100

    Like and dislike buttons

--- src/app/componentes/movie-details/movie-details.component.ts
+++ src/app/componentes/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { MovieService } from 'src/app/service/movie.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -20,7 +20,9 @@ export class MovieDetailsComponent implements OnInit {
   constructor(
     private movieService: MovieService,
     private activatedRoute: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private renderer: Renderer2,
+    private el: ElementRef) { }
 
   ////Implements
 
@@ -43,5 +45,26 @@ export class MovieDetailsComponent implements OnInit {
     this.location.back();
   }
 
+  ///////////////////////////////Renderer examples button Like/Dislike
+
+
+  @ViewChild('like', { static: true }) like: ElementRef;
+  @ViewChild('dislike', { static: true }) dislike: ElementRef;
+
+  numberlike:number = 0;
+  numberdislike:number = 0;
+
+
+  exampleLike() {
+    this.renderer.setStyle(this.like.nativeElement, 'background', '#9FE49E');
+    this.renderer.setStyle(this.dislike.nativeElement, 'display', 'none');
+    this.numberlike++;
+  }
+
+  exampleDislike() {
+    this.renderer.setStyle(this.dislike.nativeElement, 'background', '#E49E9E');
+    this.renderer.setStyle(this.like.nativeElement, 'display', 'none');
+    this.numberdislike++;
+  }
 
 }
